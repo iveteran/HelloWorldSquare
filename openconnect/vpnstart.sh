a12deed66c50bd03b7e3113f2b19120b07947595
iveteran <iveteran.yuu@gmail.com>
2016-11-06 10:22:12 +0800

    Delete useless files

--- openconnect/vpnstart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#vpnserver="vpn.idirect.net"
-vpnserver="208.226.76.25"
-# connect to vpn at 
-#openconnect -b -s /etc/vpnc/vpnc-script --authgroup=Pioneer $vpnserver</etc/myvpn.conf
-sudo /usr/local/sbin/openconnect -b --no-cert-check --authgroup=Pioneer --reconnect-timeout=500 $vpnserver </etc/myvpn.conf
