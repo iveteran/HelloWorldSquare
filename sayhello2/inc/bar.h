d16340fa62415ed96960cfe6edec5dc7e77d7fc6
Yu <yu@matrix.works>
2021-11-25 18:05:20 +0800

    make reorganization

--- sayhello2/inc/bar.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BAR_H
-#define __BAR_H
-
-void bar(int a, char *b);
-
-#endif
--- sayhello2/src/bar.cpp
+++
@@ -1,8 +0,0 @@
-#include "bar.h"
-#include <stdio.h>
-
-void bar(int a, char *b)
-{
-    printf("bar: %d, %s\n", a, b);
-    return;
-}
