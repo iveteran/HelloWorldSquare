cb058a072ebb944b6bb3688e82f6b3d331d4205e
yuu <yu@matrix.works>
2023-04-03 12:31:23 +0800

    add some demo of ChatGPT

---
+++ chatgpt/demo_chatgpt_api.py
@@ -0,0 +1,22 @@
+# Ref: https://www.cnblogs.com/botai/p/python_chatgpt_api.html
+import openai
+
+prompt = "2+2=?"
+# 设置 API 密钥
+apikey = "sk-RJqGQyLC0htwxsxxJmnST2BlbkFJlwGUvfRsPLGvK1ymG522"  #
+
+# 调用 ChatGPT
+response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                # {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+                # {"role": "assistant", "content": "https://www.cnblogs.com/botai/"},
+            ],
+            temperature=0.2,
+        )
+
+# 输出响应结果
+# print(response)
+answer = response.choices[0].message.content.strip()
+print(answer)
---
+++ chatgpt/demo_chatgpt_api_http.py
@@ -0,0 +1,37 @@
+# Ref: https://www.cnblogs.com/botai/p/python_chatgpt_api.html
+import requests
+import os,json
+
+# 设置 API 密钥和 API URL
+apikey = "sk-RJqGQyLC0htwxsxxJmnST2BlbkFJlwGUvfRsPLGvK1ymG522"  #
+url = "https://api.openai.com/v1/chat/completions"
+
+# 设置请求头
+headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + apikey,
+        }
+        
+# 设置请求体
+prompt = "3+3=?"
+field = {
+    "model":"gpt-3.5-turbo",
+    "messages":[
+        # {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+        # {"role": "assistant", "content": "https://www.cnblogs.com/botai/"}
+    ]
+}
+
+# 发送 HTTP POST 请求
+response = requests.post(url, headers=headers, data=json.dumps(field))
+
+#proxies = {
+#        "http":"http://198.11.175.192:7302"
+#        }
+# response = requests.post(url, headers=headers, proxies=proxies, data=json.dumps(field)) #使用代理
+
+# 解析响应结果
+result = response.json()
+print(result)
+print(result["choices"][0]["message"]["content"].strip())
