876f296f6f3fb13e41b2db647d66965074aae192
yuu <yu@matrix.works>
2023-06-16 09:18:55 +0800

    improve the script of showing cert info

--- security/show_cert_info_from_host.sh
+++ security/show_cert_info_from_host.sh
@@ -1,1 +1,14 @@
-openssl s_client -connect matrixworks.cn
+#echo matrixworks.cn:443
+#openssl s_client -connect matrixworks.cn 2>/dev/null | openssl x509 -noout -dates
+
+# imaps
+echo -- mail.iveteran.me/imaps
+openssl s_client -connect mail.iveteran.me:993 2>/dev/null | openssl x509 -noout -dates
+
+# pop3s
+#echo -- mail.iveteran.me/pop3s
+#openssl s_client -connect mail.iveteran.me:995 2>/dev/null | openssl x509 -noout -dates
+
+# smtps
+#echo -- mail.iveteran.me/smtps
+#openssl s_client -connect mail.iveteran.me:465 2>/dev/null | openssl x509 -noout -dates
