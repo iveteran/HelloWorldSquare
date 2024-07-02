9f35a2146d9a17b0bf45346d6b286e5c68d6f616
Yu <yu@matrix.works>
2024-07-02 15:59:00 +0800

    add config file of FZF

---
+++ dotfiles/_fzfrc.sh
@@ -0,0 +1,69 @@
+# fzf
+# Install: apt install fzf
+# Set up fzf key bindings and fuzzy completion
+# Official: https://github.com/junegunn/fzf/
+# Guide: https://thevaluable.dev/practical-guide-fzf-example/
+#eval "$(fzf --bash)"
+
+_find_exclude_dir_opt='-not -path "*__pycache__*" -not -path "*node_modules*" -not -path "*target*" -not -path "*/\.*"'
+find_in_dir() {
+    local dir=$1
+    find $dir ${_find_exclude_dir_opt}  # Error: this method of using variable is not avaiable
+}
+find_files() {
+    local dir=$1
+    #find $dir -type f ${_find_exclude_dir_opt}
+    find $dir -type f -not -path "*__pycache__*" -not -path "*node_modules*" -not -path "*target*" -not -path "*/\.*"
+}
+find_dirs() {
+    local dir=$1
+    #find $dir -type d ${_find_exclude_dir_opt}
+    find $dir -type d -not -path "*__pycache__*" -not -path "*node_modules*" -not -path "*target*" -not -path "*/\.*"
+}
+
+#export FZF_DEFAULT_COMMAND='find_in_dir .'
+#export FZF_DEFAULT_OPTS='--margin=3% --border=double --color="dark,fg:magenta"'
+export FZF_DEFAULT_OPTS='--margin=3% --border --color="dark,fg:magenta"'
+
+# Set FZF_ALT_C_COMMAND to override the default command
+#export FZF_ALT_C_COMMAND='cd'
+# Set FZF_ALT_C_OPTS to pass additional options
+
+alias find_project_files='find_files ~/projects'
+alias find_project_dirs='find_dirs ~/projects'
+alias find_cwd_files='find_files .'
+alias find_cwd_dirs='find_dirs .'
+
+alias fzf_file_preview_and_edit_on_new_tmux_window='fzf --preview "batcat --color=always {}" --bind "enter:become(tmux neww -n 'fzf-vim' vim {})"'
+alias fzf_file_edit='fzf --bind "enter:become(vim {})"'
+alias fzf_dir_preview='fzf --preview "tree -C {}"'
+
+alias fpf='find_project_files | fzf_file_preview_and_edit_on_new_tmux_window'
+alias fcf='find_cwd_files | fzf_file_preview_and_edit_on_new_tmux_window'
+alias tfpf='tmux popup -w 60% -h 60% "find_project_files | fzf_file_edit"'
+alias fpd='cd $(find_project_dirs | fzf_dir_preview)'
+alias fcd='cd $(find_cwd_dirs | fzf_dir_preview)'
+alias vim-find='vim $(fzf-tmux -p 60%,60%)'
+alias vim-find-bt='vim $(fzf-tmux)'
+
+fzf-dir() {
+    local target_dir='.'  # default
+    if [[ $# > 0 ]]; then
+        target_dir=$1
+    fi
+    find_files $target_dir | fzf_file_preview_and_edit_on_new_tmux_window
+}
+
+# Shell Integration
+#  refer: https://thevaluable.dev/fzf-shell-integration/
+source /usr/share/doc/fzf/examples/key-bindings.bash
+# Keystroke	Description
+# CTRL-t	Fuzzy find all files and subdirectories of the working directory, and output the selection to STDOUT.
+# ALT-c	    Fuzzy find all subdirectories of the working directory, and run the command “cd” with the output as argument.
+# CTRL-r	Fuzzy find through your shell history, and output the selection to STDOUT.
+
+# Completion Using fzf
+source /usr/share/bash-completion/completions/fzf
+# vim **<TAB>
+# cd **<TAB>
+# cd public**<TAB>
