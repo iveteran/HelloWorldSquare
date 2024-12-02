b38acb15938e1af44f8900ca2457ba54e67d6336
Yu <yu@matrix.works>
2024-12-02 09:37:22 +0800

    add some utility scripts

---
+++ scripts/enable_tty_2_3.sh
@@ -0,0 +1,4 @@
+sudo systemctl start getty@tty2.service
+sudo systemctl start getty@tty3.service
+
+# Press Ctrl + Alt + F2 to goto created TTY (tty2, tty3)
---
+++ scripts/start-fbterm.sh
@@ -0,0 +1,3 @@
+#fbterm -s 20 --font-names='Noto Color Emoji'
+#fbterm -s 20 --font-names='EmojiOne Color'
+fbterm -s 20 --font-names='Hack'
