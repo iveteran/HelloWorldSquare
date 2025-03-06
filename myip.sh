32ae26fb8f2ad663565f46d48f995fb775859937
Yu <yu@matrix.works>
2025-03-06 13:11:18 +0800

    improve myip.sh

--- myip.sh
+++ myip.sh
@@ -1,7 +1,12 @@
 #!/bin/env sh
 
-echo "Via ipinfo.io:"
+echo "Via ipinfo.io (使用环境变量http_proxy或https_proxy):"
 curl -s ipinfo.io
+
+echo "\n------------------"
+
+echo "Via ipinfo.io (指定Proxy服务地址):"
+curl -x http://localhost:7890 -s ipinfo.io
 
 echo "\n------------------"
 
