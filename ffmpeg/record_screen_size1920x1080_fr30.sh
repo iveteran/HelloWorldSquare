33eb18dfb02ac105d0ab34b9735ef5305f4a796c
Yu <yu@matrix.works>
2025-03-12 11:47:02 +0800

    modify the script of recording screen by ffmpeg

--- ffmpeg/record_screen_size1920x1080_fr30.sh
+++ ffmpeg/record_screen_size1920x1080_fr30.sh
@@ -2,7 +2,7 @@
 
 #DATE=`date --iso-8601='seconds'`
 DATE=`date +"%Y%m%d_%H%M%S"`
-OUTPUT_FILE="yuu-screen-capture-${DATE}.mkv"
+OUTPUT_FILE="yuu-screen-capture-${DATE}.mp4"
 echo "output to $OUTPUT_FILE"
 
 ffmpeg -video_size 1920x1080 -framerate 30 -f x11grab -i :0.0 -f pulse -ac 2 -i default $OUTPUT_FILE
