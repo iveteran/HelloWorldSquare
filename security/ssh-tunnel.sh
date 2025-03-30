ae1c659f10f6e0b9c4441f050a9ba4915fa9d3bf
Yu <yu@matrix.works>
2025-03-30 22:13:59 +0800

    improve script of ssh tunnel

--- security/ssh-tunnel.sh
+++ security/ssh-tunnel.sh
@@ -1,1 +1,6 @@
-ssh -p24 -i ~/.ssh/iv.id_rsa -D 1081 -E ssh-tunnel.log -N -f yuu@vpn.iveteran.me
+#ssh -p24 -i ~/.ssh/iv.id_rsa -D 1081 -E ssh-tunnel.log -N -f mwx-us  #yuu@vpn.iveteran.me
+#ss -nltp | grep 1081
+#ssh -p24 -i ~/.ssh/iv.id_rsa -D 1081 -vvv -N yuu@vpn.iveteran.me
+
+#sshuttle --dns -vr yuu@vpn.iveteran.me 0/0 --ssh-cmd 'ssh -p24 -i ~/.ssh/iv.id_rsa'
+sshuttle --dns -vr yuu@mwx-us 0/0 --ssh-cmd 'ssh -p24 -i ~/.ssh/iv.id_rsa'
