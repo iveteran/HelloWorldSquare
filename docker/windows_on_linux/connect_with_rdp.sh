83252a0c344ad9a5f2b8420e67f763d112de7501
Yu <yu@matrix.works>
2025-12-27 13:29:12 +0800

    modify the script of connectting remote desktop with rdp

--- docker/windows_on_linux/connect_with_rdp.sh
+++ docker/windows_on_linux/connect_with_rdp.sh
@@ -1,2 +1,2 @@
 # Linux 使用 freerdp
-xfreerdp3 /v:localhost:3390 /u:Docker /p:admin /dynamic-resolution
+xfreerdp3 /v:localhost:3390 /u:Docker /p:admin /dynamic-resolution /drive:share,/tmp/share +clipboard /scale:140
